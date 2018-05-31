f6fe4adeace29fd8509e6284f8c215884ad27b32
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-31 17:08:19 -0300

    V1 completed

--- src/integration.js
+++ src/integration.js
@@ -13,7 +13,6 @@ function transformData(transform){
 // Funções que retornam a data de CI/CO.
     var dataEntrada = transformData($checkIn);
     var dataSaida = transformData($checkOut);
-
 // Adultos Select.
     var $adultos = document.getElementById("inputadulto");
     var adultosSelecionados = $adultos.options[$adultos.selectedIndex].value;
@@ -22,4 +21,14 @@ function transformData(transform){
     var kidsSelecionados = $kids.options[$kids.selectedIndex].value;
 // Promo Code.
     var $promocode = document.getElementById("promocode").value;
+// Integração com a API
+    var CheckIn = "CheckIn="+dataEntrada;
+    var CheckOut = "CheckOut="+dataSaida;
+    var codpromo = "Code="+$promocode;
+    var nAdultos = "ad="+adultosSelecionados;
+    var kidsAge = "ag="+"5"
+    var nKids = "ch="+kidsSelecionados;+"5";
+    var apiInt = "&"+CheckIn+"&"+CheckOut+"&"+codpromo+"&"+nAdultos+"&"+nKids+"&"+kidsAge;
+    // console.log(apiInt)
+    window.location.href="https://myreservations.omnibees.com/default.aspx?q=6655&NRooms=1&"+apiInt;""
 }
