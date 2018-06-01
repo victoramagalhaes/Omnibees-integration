17104517d60be843577c6f86ebd8e1c08a78a3a7
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-31 21:00:46 -0300

    Fix Repeated letter

--- src/integration.js
+++ src/integration.js
@@ -74,5 +74,5 @@ function transformData(transform){
     var nKids = "ch="+kidsSelecionados;+"5";
     var apiInt = "&"+CheckIn+"&"+CheckOut+"&"+codpromo+"&"+nAdultos+"&"+nKids;
     // console.log(apiInt)
-    window.location.href="https://myreservations.omnibees.com/default.aspx?q=6655&NRooms=1&"+apiInt;""
+    window.location.href="https://myreservations.omnibees.com/default.aspx?q=6655&NRooms=1"+apiInt;""
 }
